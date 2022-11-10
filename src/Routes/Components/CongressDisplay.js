5aed5465c8194ad9817a08c008896c237a04697d
kainklob <klobk23@gmail.com>
2022-11-10 11:14:17 -0500

    added styling for app-baseState congressDisplay. Created base css media template to pull from. Mobile-first styling has started

--- src/Routes/Components/CongressDisplay.js
+++ src/Routes/Components/CongressDisplay.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import './CongressDisplay.css';
 
 function CongressDisplay() {
 
@@ -13,13 +14,13 @@ function CongressDisplay() {
     const [state, setState] = useState(null);
 
     return (
-        <Container id="CongressDisplay" style={{ height: '700px', marginTop: '20px', border: '1px solid black', borderRadius: '15px', padding: '10px 5px 5px 5px' }}>
+        <Container id="CongressDisplay">
             {state ? <Row className="justify-content-center">
                 <Col xs={8} className="text-center d-grid">
                     <Button onClick={() => setState(null)} size="lg">Return to States</Button>
                 </Col>
             </Row> : null}
-            <Row className="justify-content-center" style={{ marginTop: '5px' }}>
+            <Row className="justify-content-center">
                 <Col xs={12}>
                     {/* If a state is chosen, display StateMembers, otherwise display States */}
                     {state ? <StateMembers state={state} /> : <States setState={setState} />}
--- src/Routes/Components/StateCard.js
+++ src/Routes/Components/StateCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import './StateCard.css';
 
 function StateCard({ state, handleClick }) {
 
     return (
-        <Col style={{ marginTop: '10px' }}>
-            <Card style={{ width: '150px', height: '60px', cursor: 'pointer' }}>
+        <Col xs={12} md={4} lg={3} xl={2} className="StateCard" style={{ marginTop: '10px' }}>
+            <Card style={{ cursor: 'pointer' }}>
                 <Card.Body className="text-center">
-                    <Card.Title style={{ fontSize: '15px', }}>{state.name}</Card.Title>
+                    <Card.Title>{state.name}</Card.Title>
+                    <Card.Subtitle>| D: {state.partys.D} | R: {state.partys.R} | I: {state.partys.ID} |</Card.Subtitle>
                     <Card.Link onClick={handleClick} className="stretched-link" data-code={state.code} />
                 </Card.Body>
             </Card>
